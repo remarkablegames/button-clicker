477b6a2037258240be8795af761a717dd1472416
Mark <mark@remarkablemark.org>
2019-03-11 23:39:35 -0400

    feat(src): render heading, counter, and button

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import './index.css';
 
 const game = document.getElementById('game');
 
-game.innerHTML = `
-  <h1>Welcome to <code>button-clicker</code>.</h1>
-  <a href="https://github.com/remarkablegames/button-clicker" target="_blank" rel="noopener noreferrer">
-    View Source
-  </a>
-`;
+const heading = document.createElement('h1');
+heading.innerText = 'Button Clicker';
+
+const counter = document.createElement('p');
+counter.innerText = '0';
+
+const button = document.createElement('button');
+button.innerText = 'Click Button';
+
+[heading, counter, button].forEach(element => game.appendChild(element));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
