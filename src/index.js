72349418b718b3338f03f53f8dce76d2e0b153eb
Mark <mark@remarkablemark.org>
2019-03-13 21:46:05 -0400

    refactor: move logic from index.js to index.html

--- src/index.js
+++ src/index.js
@@ -3,22 +3,13 @@ import './index.css';
 
 let clicks = 0;
 
-const game = document.getElementById('game');
+const counter = document.getElementById('counter');
+const button = document.getElementById('button');
 
-const heading = document.createElement('h1');
-heading.innerText = 'Button Clicker'.toLocaleString();
-
-const counter = document.createElement('p');
-counter.innerText = clicks.toLocaleString();
-
-const button = document.createElement('button');
-button.innerText = 'Click Button'.toLocaleString();
 button.addEventListener('click', () => {
   clicks++;
   counter.innerText = clicks.toLocaleString();
 });
-
-[heading, counter, button].forEach(element => game.appendChild(element));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
