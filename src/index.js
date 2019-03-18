941de7d526136f441f5ac9e669cbf53f7ff4c330
Mark <mark@remarkablemark.org>
2019-03-18 00:44:14 -0400

    feat(src): render cursor data in store table

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const state = {
   cursor: {
     cost: 15,
     output: 1,
+    multiplier: 1.15,
+    owned: 0,
   },
 };
 
@@ -17,12 +19,33 @@ const state = {
 const elements = {
   button: document.getElementById('button'),
   counter: document.getElementById('counter'),
+  store: document.getElementById('store'),
+  storeCursor: document.getElementById('cursor'),
 };
 
 // views
 const views = {
+  renderAll: () => {
+    for (let methodName in views) {
+      if (methodName !== 'renderAll') {
+        views[methodName]();
+      }
+    }
+  },
   renderCounter: () => {
     elements.counter.innerText = state.clicks.toLocaleString();
+  },
+  renderStoreCursor: () => {
+    const { storeCursor } = elements;
+    const { cursor } = state;
+    storeCursor.querySelector(
+      '.owned'
+    ).innerText = cursor.owned.toLocaleString();
+    storeCursor.querySelector('.cost').innerText = cursor.cost.toLocaleString();
+    const nextOutput = Math.ceil(cursor.output * cursor.multiplier);
+    storeCursor.querySelector(
+      '.output'
+    ).innerText = `${nextOutput.toLocaleString()} per click`;
   },
 };
 
@@ -38,6 +61,9 @@ const actions = {
 elements.button.addEventListener(CLICK_EVENT, () => {
   actions.increment(state.cursor.output);
 });
+
+// initialize
+views.renderAll();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
