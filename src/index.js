444a1c950ee8d2d81ec19572309f8080d05e3018
Mark <mark@remarkablemark.org>
2019-04-02 00:43:38 -0400

    refactor(src): change `state.generator` into `state.generators`
    Generators is now a map of id to data, which makes it easier to
    add more generators.
    
    Update views, actions, and events given refactor.

--- src/index.js
+++ src/index.js
@@ -24,18 +24,21 @@ const state = {
       base: 1,
     },
   },
-  generator: {
-    owned: 0,
-    delay: 10, // in seconds
-    cost: {
-      next: 50,
-      base: 50,
-      rate: 1.1,
-    },
-    output: {
-      current: 0,
-      next: 1,
-      base: 1,
+  generators: {
+    generator: {
+      label: 'Generator',
+      owned: 0,
+      delay: 10, // in seconds
+      cost: {
+        next: 50,
+        base: 50,
+        rate: 1.1,
+      },
+      output: {
+        current: 0,
+        next: 1,
+        base: 1,
+      },
     },
   },
   intervals: {},
@@ -48,10 +51,12 @@ const elements = {
   store: document.getElementById('store'),
   storeCursor: document.getElementById('cursor'),
 };
-elements.storeGenerator = elements.storeCursor.cloneNode(true);
-elements.storeGenerator.id = '';
-elements.storeGenerator.querySelector(BUTTON).innerText = 'Generator';
-elements.store.appendChild(elements.storeGenerator);
+Object.keys(state.generators).forEach(id => {
+  const generatorNode = elements.storeCursor.cloneNode(true);
+  generatorNode.id = id;
+  generatorNode.querySelector(BUTTON).innerText = state.generators[id].label;
+  elements.store.appendChild(generatorNode);
+});
 
 /** Helpers. */
 
@@ -85,6 +90,7 @@ const views = {
   renderCounter: () => {
     elements.counter.innerText = state.clicks.current.toLocaleString();
   },
+
   renderStoreCursor: () => {
     const { storeCursor } = elements;
     const { cursor } = state;
@@ -98,19 +104,29 @@ const views = {
       '.output'
     ).innerText = `${cursor.output.next.toLocaleString()} per click`;
   },
-  renderStoreGenerator: () => {
-    const { storeGenerator } = elements;
-    const { generator } = state;
-    storeGenerator.querySelector(
+
+  /**
+   * @param {String} id
+   */
+  renderStoreGenerator: id => {
+    const generator = state.generators[id];
+    const generatorRow = document.getElementById(id);
+    generatorRow.querySelector(
       '.owned'
     ).innerText = generator.owned.toLocaleString();
-    storeGenerator.querySelector(
+    generatorRow.querySelector(
       '.cost'
     ).innerText = generator.cost.next.toLocaleString();
-    storeGenerator.querySelector('.output').innerText = formatGeneratorOutput(
+    generatorRow.querySelector('.output').innerText = formatGeneratorOutput(
       generator.output.next,
       generator.delay
     );
+  },
+
+  renderStoreGenerators: () => {
+    Object.keys(state.generators).forEach(id => {
+      views.renderStoreGenerator(id);
+    });
   },
 };
 
@@ -126,11 +142,17 @@ const actions = {
     if (!skipTotal) {
       clicks.total += number;
     }
+
     views.renderCounter();
     const cursorButton = elements.storeCursor.querySelector(BUTTON);
     cursorButton.disabled = clicks.current < state.cursor.cost.next;
-    const generatorButton = elements.storeGenerator.querySelector(BUTTON);
-    generatorButton.disabled = clicks.current < state.generator.cost.next;
+
+    const { generators } = state;
+    Object.keys(generators).forEach(id => {
+      const generatorRow = document.getElementById(id);
+      const generatorButton = generatorRow.querySelector(BUTTON);
+      generatorButton.disabled = clicks.current < generators[id].cost.next;
+    });
   },
 
   /**
@@ -150,14 +172,17 @@ const actions = {
     views.renderStoreCursor();
   },
 
-  updateStoreGenerator: () => {
-    const { generator } = state;
+  /**
+   * @param {String} id
+   */
+  updateStoreGenerator: id => {
+    const generator = state.generators[id];
     const { cost, output } = generator;
     const owned = ++generator.owned;
     cost.next = calculateNextCost(cost.base, cost.rate, owned);
     output.current = output.next;
     output.next = Math.round(output.base * (owned + 1));
-    views.renderStoreGenerator();
+    views.renderStoreGenerator(id);
   },
 };
 
@@ -177,29 +202,37 @@ elements.storeCursor.querySelector(BUTTON).addEventListener(CLICK, () => {
 });
 
 // purchase generator
-elements.storeGenerator.querySelector(BUTTON).addEventListener(CLICK, () => {
-  if (state.clicks.current >= state.generator.cost.next) {
-    const { generator, intervals } = state;
-    actions.decrement(generator.cost.next);
-    actions.updateStoreGenerator();
+Object.keys(state.generators).forEach(id => {
+  const generator = state.generators[id];
+  const generatorRow = document.getElementById(id);
 
-    if (intervals.generator) {
-      intervals.generator.callback = () =>
-        actions.increment(generator.output.current);
-    } else {
-      intervals.generator = {
-        output: generator.output.current,
-        callback: () => actions.increment(generator.output.current),
-      };
-      setInterval(intervals.generator.callback, generator.delay * 1000);
+  generatorRow.querySelector(BUTTON).addEventListener(CLICK, () => {
+    if (state.clicks.current >= generator.cost.next) {
+      const { intervals } = state;
+      actions.decrement(generator.cost.next);
+      actions.updateStoreGenerator(id);
+
+      if (intervals.generator) {
+        intervals.generator.callback = () => {
+          actions.increment(generator.output.current);
+        };
+      } else {
+        intervals.generator = {
+          output: generator.output.current,
+          callback: () => {
+            actions.increment(generator.output.current);
+          },
+        };
+        setInterval(intervals.generator.callback, generator.delay * 1000);
+      }
     }
-  }
+  });
 });
 
 /** Bootstrap. */
 views.renderCounter();
 views.renderStoreCursor();
-views.renderStoreGenerator();
+views.renderStoreGenerators();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
