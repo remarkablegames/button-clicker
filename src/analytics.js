3c897c116915f73aab4d0597b33c7447ec40d81b
Mark <mark@remarkablemark.org>
2022-03-27 18:49:51 -0400

    refactor(public): move some analytics logic back to index.html

--- src/analytics.js
+++ src/analytics.js
@@ -4,14 +4,7 @@
  * https://developers.google.com/analytics/devguides/collection/gtagjs
  */
 
-window.dataLayer = window.dataLayer || [];
-
-function gtag() {
-  window.dataLayer.push(arguments);
-}
-
-gtag('js', new Date());
-gtag('config', process.env.GOOGLE_ANALYTICS_ID);
+window.gtag('config', process.env.GOOGLE_ANALYTICS_ID);
 
 window[`ga-disable-${process.env.GOOGLE_ANALYTICS_ID}`] =
   process.env.NODE_ENV !== 'production';
