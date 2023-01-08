661735feddfecca88a01d302ab087ee54bd907c3
Mark <mark@remarkablemark.org>
2023-01-07 22:51:52 -0500

    refactor(src): refactor helpers.js to helpers.ts

---
+++ src/helpers.ts
@@ -0,0 +1,65 @@
+import { SELECTOR_BUTTON } from './constants';
+
+/**
+ * Elements.
+ */
+const cursor = document.getElementById('cursor')!;
+
+export const elements = {
+  button: document.getElementById('button')!,
+  counter: document.getElementById('counter')!,
+  cursor,
+  cursorButton: cursor.querySelector(SELECTOR_BUTTON)!,
+  message: document.getElementById('message')!,
+  store: document.getElementById('store')!,
+};
+
+/**
+ * Calculates next cost of cursor or generator upgrade.
+ *
+ * @param base - Current base.
+ * @param rate - Current rate.
+ * @param owned - Amount owned.
+ * @returns - Next cost.
+ */
+export const calculateNextCost = (
+  base: number,
+  rate: number,
+  owned: number
+): number => Math.floor(base * Math.pow(rate, owned));
+
+/**
+ * Gets element by id (from cache if applicable).
+ *
+ * @param id - Element id.
+ * @returns - Element.
+ */
+export function getElementById(id: keyof typeof elements): HTMLElement {
+  if (elements[id]) {
+    return elements[id];
+  }
+  return ((elements[id] as HTMLElement) = document.getElementById(id)!);
+}
+
+/**
+ * Formats generator output description.
+ *
+ * @example
+ * // returns '1 click per 1 second'
+ * formatGeneratorOutput(1, 1);
+ *
+ * @example
+ * // returns '2 clicks per 2 seconds'
+ * formatGeneratorOutput(2, 2);
+ *
+ * @param clicks - Clicks.
+ * @param seconds - Seconds.
+ * @returns - Description.
+ */
+export const formatGeneratorOutput = (
+  clicks: number,
+  seconds: number
+): string =>
+  `${clicks.toLocaleString()} ${
+    clicks === 1 ? 'click' : 'clicks'
+  } per ${seconds.toLocaleString()} ${seconds === 1 ? 'second' : 'seconds'}`;
