9c9855c062deb377ea4b091f72c9d6e4b1dc03c9
Mark <mark@remarkablemark.org>
2023-01-08 17:45:09 -0500

    refactor(views): rename cursor.js to cursor.ts

--- src/views/cursor.ts
+++ src/views/cursor.ts
@@ -4,18 +4,22 @@ import * as state from '../state';
 /**
  * Renders cursor.
  */
-export function renderCursor() {
+export function renderCursor(): void {
   const cursorRow = elements.cursor;
   const { cursor } = state;
-  cursorRow.querySelector('.owned').innerText = (
+
+  (cursorRow.querySelector('.owned') as HTMLElement).innerText = (
     cursor.owned - 1
   ).toLocaleString();
-  cursorRow.querySelector('.cost').innerText =
+
+  (cursorRow.querySelector('.cost') as HTMLElement).innerText =
     cursor.cost.next.toLocaleString();
-  cursorRow.querySelector(
-    '.output-current'
+
+  (
+    cursorRow.querySelector('.output-current') as HTMLElement
   ).innerText = `${cursor.output.current.toLocaleString()} per click`;
-  cursorRow.querySelector(
-    '.output-next'
+
+  (
+    cursorRow.querySelector('.output-next') as HTMLElement
   ).innerText = `${cursor.output.next.toLocaleString()} per click`;
 }
