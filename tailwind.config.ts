5715cde587dfa438b250a50e6fbfe74a7c7d6cf7
Mark <mark@remarkablemark.org>
2024-12-21 13:22:47 -0500

    build(tailwind): init tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./index.html', './src/**/*.{js,ts,mjs,mts,jsx,tsx}'],
+  plugins: [],
+  theme: {
+    extend: {},
+  },
+};
+
+export default config;
